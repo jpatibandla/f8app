97759ea7c989ace199ad3be0aba893d01469b265
Alex Kotliarskyi <alex.frantic@gmail.com>
2016-03-25 20:30:15 -0700

    Move Friend's schedule view to new API

--- js/tabs/schedule/FriendsScheduleView.js
+++ js/tabs/schedule/FriendsScheduleView.js
@@ -23,44 +23,25 @@
  */
 'use strict';
 
-var F8SegmentedControl = require('F8SegmentedControl');
-var F8SessionCell = require('F8SessionCell');
-var Image = require('Image');
 var Navigator = require('Navigator');
 var ProfilePicture = require('../../common/ProfilePicture');
 var React = require('React');
 var EmptySchedule = require('./EmptySchedule');
 var FilterSessions = require('./filterSessions');
 var ListContainer = require('ListContainer');
-var groupSessions = require('./groupSessions');
-var SessionsSectionHeader = require('./SessionsSectionHeader');
+var ScheduleListView = require('./ScheduleListView');
 
 var { connect } = require('react-redux');
 
-
 import type {Session} from '../../reducers/sessions';
 import type {FriendsSchedule} from '../../reducers/friendsSchedules';
-import type {SessionsListData} from './groupSessions';
 
 var { createSelector } = require('reselect');
-
-const data = createSelector(
-  (store) => store.sessions,
-  (store, props) => props.friend.schedule,
-  (sessions, schedule) => {
-    const sessionsInSchedule = FilterSessions.bySchedule(sessions, schedule);
-    return [
-      groupSessions(FilterSessions.byDay(sessionsInSchedule, 1)),
-      groupSessions(FilterSessions.byDay(sessionsInSchedule, 2)),
-    ];
-  }
-);
 
 type Props = {
   sessions: Array<Session>;
   friend: FriendsSchedule;
   navigator: Navigator;
-  data: Array<SessionsListData>;
 };
 
 class FriendsScheduleView extends React.Component {
@@ -68,89 +49,59 @@ class FriendsScheduleView extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.state = { page: 0 };
-
-    this.renderStickyHeader = this.renderStickyHeader.bind(this);
-    this.renderSectionHeader = this.renderSectionHeader.bind(this);
-    this.renderRow = this.renderRow.bind(this);
     this.renderEmptyList = this.renderEmptyList.bind(this);
-    this.openSession = this.openSession.bind(this);
-    this.selectDay = this.selectDay.bind(this);
   }
 
   render() {
+    const backItem = {
+      icon: require('../../common/img/back_white.png'),
+      onPress: () => this.props.navigator.pop(),
+    };
     const firstName = this.props.friend.name.split(' ')[0];
     return (
       <ListContainer
         title={`${firstName}'s Schedule`}
         parallaxContent={<ProfilePicture userID={this.props.friend.id} size={100} />}
         backgroundImage={require('./img/schedule-background.png')}
-        backgroundShift={this.state.page}
         backgroundColor={'#A8D769'}
-        data={this.props.data[this.state.page]}
-        renderStickyHeader={this.renderStickyHeader}
-        renderSectionHeader={this.renderSectionHeader}
-        renderRow={this.renderRow}
-        renderEmptyList={this.renderEmptyList}
-        leftItem={{
-          icon: require('../../common/img/back_white.png'),
-          onPress: () => this.props.navigator.pop(),
-        }}
-      />
+        leftItem={backItem}>
+        <ScheduleListView
+          title="Day 1"
+          day={1}
+          sessions={this.props.sessions}
+          renderEmptyList={this.renderEmptyList}
+          navigator={this.props.navigator}
+        />
+        <ScheduleListView
+          title="Day 2"
+          day={2}
+          sessions={this.props.sessions}
+          renderEmptyList={this.renderEmptyList}
+          navigator={this.props.navigator}
+        />
+      </ListContainer>
     );
   }
 
-  renderEmptyList() {
+  renderEmptyList(day) {
     return (
       <EmptySchedule
         title="Nothing to show."
-        text={`${this.props.friend.name} has not added any sessions for day ${this.state.page + 1}`}
+        text={`${this.props.friend.name} has not added any sessions for day ${day}`}
       />
     );
-  }
-
-  renderStickyHeader() {
-    return (
-      <F8SegmentedControl
-        values={['Day 1', 'Day 2']}
-        selectedIndex={this.state.page}
-        selectionColor="white"
-        onChange={this.selectDay}
-      />
-    );
-  }
-
-  renderSectionHeader(sectionData, sectionID) {
-    return <SessionsSectionHeader title={sectionID} />;
-  }
-
-  renderRow(session) {
-    var route = {
-      day: this.state.page + 1,
-      session: session,
-      allSessions: this.props.data[this.state.page],
-    };
-    return (
-      <F8SessionCell
-        onPress={() => this.openSession(route)}
-        session={session}
-      />
-    );
-  }
-
-  openSession(route) {
-    this.props.navigator.push(route);
-  }
-
-  selectDay(page) {
-    this.setState({page});
   }
 }
 
+const data = createSelector(
+  (store) => store.sessions,
+  (store, props) => props.friend.schedule,
+  (sessions, schedule) => FilterSessions.bySchedule(sessions, schedule),
+);
+
 function select(store, props) {
   return {
-    data: data(store, props),
+    sessions: data(store, props),
   };
 }
 
